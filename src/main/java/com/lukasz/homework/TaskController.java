6ac98c4799f2779a5800c57f2dc1e67023e71ee6
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-07-31 23:24:22 +0200

    Added first page html.

--- src/main/java/com/lukasz/homework/TaskController.java
+++ src/main/java/com/lukasz/homework/TaskController.java
@@ -1,10 +1,27 @@
 package com.lukasz.homework;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Controller
 public class TaskController {
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @GetMapping("/")
+    public String toDoTasks(Model model){
+        TypedQuery<Task> query= entityManager.createQuery("select m from Task m where m.is_done='no'", Task.class);
+        List<Task> tasksList = query.getResultList();
+        model.addAttribute("tasks",tasksList);
+        return "home";
+    }
 
 
 }
