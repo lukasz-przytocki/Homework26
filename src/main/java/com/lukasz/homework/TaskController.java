bec271e8fa66a0cee788a75686793dbb995026fd
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-08-01 23:12:42 +0200

    Nie działa konstruktor z datą.

--- src/main/java/com/lukasz/homework/TaskController.java
+++ src/main/java/com/lukasz/homework/TaskController.java
@@ -1,27 +1,70 @@
 package com.lukasz.homework;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
 public class TaskController {
 
     @Autowired
-    private EntityManager entityManager;
+    private EntityManagerFactory entityManagerFactory;
+;
 
     @GetMapping("/")
-    public String toDoTasks(Model model){
-        TypedQuery<Task> query= entityManager.createQuery("select m from Task m where m.is_done='no'", Task.class);
-        List<Task> tasksList = query.getResultList();
-        model.addAttribute("tasks",tasksList);
+    public String home(){
         return "home";
     }
 
+    @GetMapping("/notDone")
+    public String toDoTasks(Model model){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<Task> query= entityManager.createQuery("select t from Task t  where t.isDone=:isDone order by t.toDate asc", Task.class);
+         query.setParameter("isDone", "no");
+        List<Task> tasksList = query.getResultList();
+        model.addAttribute("tasks",tasksList);
+        return "notDone";
+    }
+    @GetMapping("/archive")
+    public String archiveTasks(Model model){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<Task> query= entityManager.createQuery("select t from Task t  where t.isDone=:isDone order by t.toDate desc", Task.class);
+        query.setParameter("isDone", "yes");
+        List<Task> tasksList = query.getResultList();
+        model.addAttribute("tasks",tasksList);
+        return "archive";
+    }
 
+    @PostMapping("/add")
+    public String add(Model model, @RequestParam String task, @DateTimeFormat(iso=DateTimeFormat.ISO.DATE)  @ModelAttribute LocalDate toDate, TaskCategory category){
+        Task task1 = new Task(task,toDate,category, "no");
+        EntityManager entityManager= entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(task1);
+        entityManager.getTransaction().commit();
+       // return "dodano!";
+//        TypedQuery<Task> query= entityManager.createQuery("insert into Task (category,is_done, task, to_date) VALUES (:category,:isDone,:task,:toDate)", Task.class);
+//        query.setParameter("category", category);
+//        query.setParameter("isDone", "no");
+//        query.setParameter("task", task);
+//        query.setParameter("toDate", toDate);
+
+
+//        List<Task> tasksList = query.getResultList();
+//        model.addAttribute("tasks",tasksList);
+        return "home";
+    }
+ //   INSERT iNTO task(category, is_done, task, to_date)
+ //   values('HOUSE','no','Clean windows','2019-08-30'),
+
+//    "insert into Task (category,is_done, task, to_date) VALUES (:category,:isDone,:task,:toDate)", Task.class))
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,5 @@ INSERT iNTO task(category, is_done, task, to_date)
 values('HOUSE','no','Clean windows','2019-08-30'),
 ('SHOPING','yes' ,'Potatos','2019-07-31'),
 ('CAR','no','Change tire to winter one','2019-10-20'),
-('BILLS','no','Pay for electricity','2019-08-15');
+('BILLS','no','Pay for electricity','2019-08-15'),
+('OTHER','yes','Buy holidays','2019-05-15');
